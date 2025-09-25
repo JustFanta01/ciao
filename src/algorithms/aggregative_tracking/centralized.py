Fix: total gradient must be in the same dim. as decision var!
Wrong gradient summation, they need to be stacked, zz.shape=(N,d), grad.shape=(N,d)

src/algorithms/aggregative_tracking/centralized.py
@@ -20,43 +20,55 @@ class CentralizedGradientMethod(Algorithm):
         # [ define collectors ]
         collector_zz = TrajectoryCollector("zz", max_iter, (N,) + d)
         collector_cost = TrajectoryCollector("cost", max_iter, ())
-        collector_grad = TrajectoryCollector("grad", max_iter, d)
+        collector_grad = TrajectoryCollector("grad", max_iter, (N,) + d)
         collector_sigma = TrajectoryCollector("sigma", max_iter, d)
     
         for k in range(max_iter):
             sigma = self.problem.sigma()
             cost = self.problem.centralized_cost_fn()
-            total_grad = np.zeros(d)
             
+            # This list contains each i-th 
+            # block of the gradient, denoted as: $$ [\nabla_{z_i} \sum_{j=1}^{N} \ell_j(z_j, \sigma(z_1, \dots, z_N))]_i \Bigg|_{z_1 = z_1^k,\, \dots,\, z_N = z_N^k} \in \mathbb{R}^{d} $$
+            total_grad_list = []
+
             # [ new states ]
             zz_k_plus_1 = np.zeros((N,) + d)
 
             for i, agent_i in enumerate(self.problem.agents):
-                gradient_sum = np.zeros(d)
+                nabla_2_sum = np.zeros(d)
 
                 for agent_j in self.problem.agents:
-                    gradient_sum += agent_j.nabla_2(agent_j["zz"], sigma)
+                    nabla_2_sum += agent_j.nabla_2(agent_j["zz"], sigma)
 
                 # [ compute gradients ]
                 nabla_1 = agent_i.nabla_1(agent_i["zz"], sigma)
                 nabla_phi = agent_i.nabla_phi(agent_i["zz"])
-                grad_i = nabla_1 + 1/N * nabla_phi @ gradient_sum
+                
+                
+                # $$[\nabla_{z_i} \ell(z, \sigma(z))]_i$$ is equal to: $$  \nabla_1 \ell_i(z_i, \sigma) \Bigg|_{z_i = z_i^k, \sigma = \frac{1}{N}\sum_{j=1}^N \phi_j(z_j^k)} + \frac{1}{N}\nabla \phi_i(z_i) \Bigg|_{z_i = z_i^k} \cdot \sum_{j=1}^N \nabla_2 \ell_j(z_j, \sigma) \Bigg|_{z_j = z_j^k, \sigma = \frac{1}{N}\sum_{j=1}^N \phi_j(z_j^k)} $$
+                
+                
+                grad_i = nabla_1 + 1/N * nabla_phi @ nabla_2_sum
+                # grad_i_f = self.problem.centralized_gradient_of_cost_fn_of_agent(i) # shape: (d,)
                 
                 # [ update zz_i ]
                 zz_k_plus_1[i] = agent_i["zz"] - stepsize * grad_i
                 zz_k_plus_1[i] = np.clip(zz_k_plus_1[i], 0, 1)   # vincolo [0,1]
                 
-                total_grad += grad_i
+                # total_grad += grad_i
+                total_grad_list.append(grad_i)
+            
+            # [ collectors ]
+            collector_zz.log(k, np.array([ag["zz"] for ag in self.problem.agents]))
+            collector_cost.log(k, cost)
+
+            total_grad = np.array(total_grad_list)
+            collector_grad.log(k, total_grad)
+            collector_sigma.log(k, sigma)
             
             # [ writeback ]
             for i, agent_i in enumerate(self.problem.agents):
                 agent_i["zz"] = zz_k_plus_1[i]
-
-            # [ collectors ]
-            collector_zz.log(k, np.array([ag["zz"] for ag in self.problem.agents]))
-            collector_cost.log(k, cost)
-            collector_grad.log(k, total_grad)
-            collector_sigma.log(k, sigma)
 
         result = RunResult(
             zz_traj=collector_zz.get(),

src/main_agg_tracking.py
@@ -17,7 +17,7 @@ from utils import graph_utils
 
 
 def main():
-    N = 2  # number of agents
+    N = 5  # number of agents
     d = 1  # dimension of the state space
     seed = 3
     rng = np.random.default_rng(seed)
@@ -47,12 +47,12 @@ def main():
     def setup_problem():
         agents = []
         for i in range(N):
-            # cost_params = LocalCloudTradeoffCostFunction.CostParams(
-            #     alpha[i], beta[i], energy_tx[i], energy_task[i], time_task[i], time_rtt[i]
-            # )
-            # cost_fn = LocalCloudTradeoffCostFunction(cost_params)
-            cost_params = QuadraticCostFunction.CostParams(cc=2 * 0.5) # optimum: z_i = 0.5
-            cost_fn = QuadraticCostFunction(cost_params)
+            cost_params = LocalCloudTradeoffCostFunction.CostParams(
+                alpha[i], beta[i], energy_tx[i], energy_task[i], time_task[i], time_rtt[i]
+            )
+            cost_fn = LocalCloudTradeoffCostFunction(cost_params)
+            # cost_params = QuadraticCostFunction.CostParams(cc=2 * 0.5) # optimum: z_i = 0.5
+            # cost_fn = QuadraticCostFunction(cost_params)
             phi_fn = IdentityFunction(d)
             # init_state = rng.uniform(0, 1, size=d)
             agent_i = Agent(i, cost_fn, phi_fn, init_state[i])
@@ -77,10 +77,15 @@ def main():
     # -----------------------
     problem = setup_problem()
     centralized = CentralizedGradientMethod(problem)
-    algo_params = CentralizedGradientMethod.AlgorithmParams(max_iter=500, stepsize=0.01, seed=seed)
+    args = {
+        "max_iter": 1000,
+        "stepsize": 0.01, 
+        "seed":seed
+    }
+    algo_params = CentralizedGradientMethod.AlgorithmParams(**args)
     result = centralized.run(algo_params)
     
-    plotter = plots.BaseRunResultPlotter(result, semilogy=True)
+    plotter = plots.BaseRunResultPlotter(result)
     plotter.plot_cost().plot_grad_norm().plot_agents_trajectories().plot_sigma_trajectory().show()
     # animation.animate_offloading_with_mean(result, problem.agents, interval=80)
 
@@ -90,10 +95,15 @@ def main():
     # -----------------------
     problem = setup_problem()
     distributed = DistributedAggregativeTracking(problem)
-    algo_params = DistributedAggregativeTracking.AlgorithmParams(max_iter=500, stepsize=0.01, seed=seed)
+    args = {
+        "max_iter": 1000,
+        "stepsize": 0.01, 
+        "seed":seed
+    }
+    algo_params = DistributedAggregativeTracking.AlgorithmParams(**args)
     result = distributed.run(algo_params)
 
-    plotter = plots.BaseRunResultPlotter(result, semilogy=True)
+    plotter = plots.BaseRunResultPlotter(result)
     plotter.plot_cost().plot_grad_norm().plot_agents_trajectories().plot_sigma_trajectory().show()
     # animation.animate_offloading_with_mean(result, problem.agents, interval=80)
 

src/models/optimization_problem.py
@@ -58,13 +58,13 @@ class OptimizationProblem():
         agent_i = self.agents[idx]
 
         # [ compute gradients ]
-        gradient_sum = np.zeros(self.d)
+        nabla_2_sum = np.zeros(self.d)
         for agent_j in self.agents:
-            gradient_sum += agent_j.nabla_2(agent_j["zz"] , sigma)
+            nabla_2_sum += agent_j.nabla_2(agent_j["zz"] , sigma)
         
         nabla_1 = agent_i.nabla_1(agent_i["zz"], sigma)
         nabla_phi = agent_i.nabla_phi(agent_i["zz"])
-        grad_i_f = nabla_1 + 1/self.N * nabla_phi @ gradient_sum
+        grad_i_f = nabla_1 + 1/self.N * nabla_phi @ nabla_2_sum
 
         return grad_i_f
 

src/viz/plots.py
@@ -36,7 +36,11 @@ class BaseRunResultPlotter:
 
     def plot_grad_norm(self, semilogy=True):
         grad = self.result.grad_traj
-        grad_norm = np.linalg.norm(grad, axis=1)
+        dims = len(grad.shape)
+        # the norm of the whole matrix / vector present at each iteration
+        # so if grad.shape = (K, N, d1, d2, d3)
+        # grad_norm[k] is the norm of the (N, d1, d2, d3) matrix at grad[k]
+        grad_norm = np.linalg.norm(grad, axis=tuple(range(1, dims)))
         K = grad_norm.shape[0]
         ax = self.axes[0, 1]
         if semilogy: